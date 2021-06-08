1b91e1f88a55bfb4a60fd40c8eee4335783fed12
hsia <yunhsia.ho@gmail.com>
2021-06-08 21:55:03 +0200

    save

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 //event is get the keydown-key's information.
-window.addEventListener('keydown', playSounds())
 
-function playSounds (event){
+
+function playSound (event){
     // console.log(event.keycode);
     const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${event.keyCode}"]`)
     console.log(audio);
-    if(!audio)return// stop the audio from running all together == stop keep running the function
+    if(!audio)return;// stop the audio from running all together == stop keep running the function
     audio.currentTime = 0; //everytime hitting the keys, it'll replay audio right away
     audio.play();
-    key.classList.add(playing);
+    key.classList.add('playing');
 }
 function removeTransition(e){
     if(e.propertyName!== 'transform') return; //skip it if it's not a transform
-    console.log(e.propertyName);
+    this.classList.remove('playing');//this is audio info
 }
 
 const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
